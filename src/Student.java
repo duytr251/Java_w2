a138511ddc9b3e6704a770caf97ee8d8fd09ca71
VMNhatJr10 <74690179+nhat123-dev@users.noreply.github.com>
2021-09-16 08:14:51 +0700

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -39,42 +39,4 @@ public class Student {
         return this.name + "-" + this.id + "-" + this.group + "-" + this.email + "\n";
     }
 
-    public static void main(String[] args) {
-        Student sv1 = new Student("vu minh nhat" , "20021407", "K65CACLC3","20021407@vnu.edu.vn");
-        //System.out.println(sv1.getInfor());
-
-        /*sv1.setName("Vu Minh Nhat");
-        System.out.println(sv1.getInfor());
-        System.out.println(sv1.getName());
-
-        Student sv2 = new Student(sv1);
-        System.out.println(sv2.getInfor());
-           */
-       // System.out.println(printHello());
-        Student sv2 = new Student("tran van duy", "20021319", "K65CACLC2","20021319@vnu.edu.vn");
-        Student sv3 = new Student("dao van quyen", "20021422", "K65CACLC2","20021422@vnu.edu.vn");
-        Student sv4 = new Student("le anh tuan", "20021234", "K65NCLC1","21231@vnu.edu.vn");
-
-        StudentManagement lophoc = new StudentManagement();
-        lophoc.addStudent(sv1);
-        lophoc.addStudent(sv2);
-        lophoc.addStudent(sv3);
-        lophoc.addStudent(sv4);
-
-        //lophoc.studentsByGroup();
-        String gr = lophoc.studentsByGroup();
-        for(int i = 0; i < gr.length(); i++){
-            System.out.print(gr.charAt(i));
-        }
-        lophoc.removeStudent("20021319");
-
-        String gr1 = lophoc.studentsByGroup();
-        for(int i = 0; i < gr1.length(); i++){
-            System.out.print(gr1.charAt(i));
-        }
-        //System.out.println(lophoc.sameGroup(sv1,sv2));
-    }
-
-
-
 }
