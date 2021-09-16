2914455a1b6e6efd8787895ccca2399e59c3fb48
VMNhatJr10 <74690179+nhat123-dev@users.noreply.github.com>
2021-09-16 08:17:24 +0700

    Update StudentManagement.java

--- src/StudentManagement.java
+++ src/StudentManagement.java
@@ -38,4 +38,43 @@ public class StudentManagement {
         }
         students.remove(students.get(pos));
     }
+    
+    public static void main(String[] args) {
+        // make object
+        Student sv1 = new Student("vu minh nhat" , "20021407", "K65CACLC3","20021407@vnu.edu.vn");
+        Student sv2 = new Student("tran van duy", "20021319", "K65CACLC2","20021319@vnu.edu.vn");
+        Student sv3 = new Student("dao van quyen", "20021422", "K65CACLC2","20021422@vnu.edu.vn");
+        Student sv4 = new Student("le anh tuan", "20021234", "K65NCLC1","21231@vnu.edu.vn");
+
+        //System.out.println(sv1.getInfor());
+        sv1.setName("Vu Minh Nhat"); // set name
+        System.out.println(sv1.getInfor()); // get information of obj
+        System.out.println(sv1.getName()); // get name of obj
+
+        Student sv5 = new Student(sv1);
+        System.out.println(sv5.getInfor());
+
+
+        // make a array object
+        StudentManagement lophoc = new StudentManagement();
+        // add student
+        lophoc.addStudent(sv1);
+        lophoc.addStudent(sv2);
+        lophoc.addStudent(sv3);
+        lophoc.addStudent(sv4);
+
+        //lophoc.studentsByGroup();
+        // print the list of student
+        String gr = lophoc.studentsByGroup();
+        for(int i = 0; i < gr.length(); i++){
+            System.out.print(gr.charAt(i));
+        }
+        lophoc.removeStudent("20021319");
+
+        String gr1 = lophoc.studentsByGroup();
+        for(int i = 0; i < gr1.length(); i++){
+            System.out.print(gr1.charAt(i));
+        }
+        //System.out.println(lophoc.sameGroup(sv1,sv2));
+    }
 }
